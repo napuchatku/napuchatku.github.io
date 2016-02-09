a7012d128fcb50d4c86b87fa2e760db96133abde
gwdo <napuchatku@gmail.com>
2016-02-09 14:30:48 +0100

    update

--- js/app.js
+++ js/app.js
@@ -1,105 +1,34 @@
 'use strict';
-      function initialize() {
+function init() {
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions),
+      infowindow = new google.maps.InfoWindow(),
+      markers = [];
 
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
+  function setMarker(i) {
+    markers[i] = new google.maps.Marker({
+      position: locationsArray[i].position,
+      title: locationsArray[i].title,
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        strokeColor: '#E83230', 
+        scale: 3
+      },
+      map: map
+    });
+  }
 
-        var infowindow = new google.maps.InfoWindow();
+  function attachInfoWindow(i) {
+    var content = '<h1>' + markers[i].title + '</h1>';
+    google.maps.event.addListener(markers[i], 'click', function() {
+      infowindow.setContent(content);
+      infowindow.open(map, markers[i]);
+    });
+  }
 
-        var markers = [];
+  for (var i = 0; i < locationsArray.length; i++) {
+    setMarker(i);
+    attachInfoWindow(i);
+  }
+}
 
-        function setMarker(i) {
-          markers[i] = new google.maps.Marker(
-            {
-              position: locationsArray[i].position,
-              title: locationsArray[i].title,
-              icon: locationsArray[i].icon,
-              map: map,
-            }
-          );
-        }
-
-        function formatDate(dateObject) {
-          var formattedDate = '';
-          formattedDate += dateObject.from.getDate();
-          formattedDate += '/';
-          formattedDate += dateObject.from.getMonth();
-          formattedDate += '/';
-          formattedDate += dateObject.from.getFullYear();
-          formattedDate += ' - ';
-          formattedDate += dateObject.to.getDate();
-          formattedDate += '/';
-          formattedDate += dateObject.to.getMonth();
-          formattedDate += '/';
-          formattedDate += dateObject.to.getFullYear();
-          return formattedDate;
-        }
-
-        function attachInfoWindow(i) {
-          var content = '<h1>' + markers[i].title + '</h1>';
-          if(locationsArray[i].dates) {
-            content += '<hr>';
-            content += '<ol>';
-            for (var k = 0; k < locationsArray[i].dates.length; k++) {
-              content += '<li>' + formatDate(locationsArray[i].dates[k]) + '</li>';
-            }
-            content += '</ol>';
-          }
-          google.maps.event.addListener(markers[i], 'click', function() {
-            infowindow.setContent(content);
-            infowindow.open(map, markers[i]);
-          });
-        }
-
-        for (var i = 0; i < locationsArray.length; i++) {
-          setMarker(i);
-          attachInfoWindow(i);
-        }
-
-            function getUpdatedScale(zoomLevel){
-              var scale; 
-              if (zoomLevel < 7) {
-                scale = .3;
-              } 
-              else if(zoomLevel < 8) {
-                scale = .5;
-              }              
-              else if(zoomLevel < 9) {
-                scale = .75;
-              }
-              else if(zoomLevel < 10) {
-                scale = .9;
-              }
-              else if(zoomLevel < 11) {
-                scale = 1;
-              }
-              else {
-                scale = 1.1;
-              };
-              return scale;
-            };
-
-        google.maps.event.addListener(map, 'zoom_changed', function() {
-            var z = map.getZoom();
-            var updatedScale = getUpdatedScale(z);
-
-            for (var i = 0; i < markers.length; i++) {
-              markers[i].setIcon(
-                {
-                path: markers[i].getIcon().path,
-                strokeColor: markers[i].getIcon().strokeColor,
-                fillColor: markers[i].getIcon().fillColor,
-                anchor: {
-                  x: markers[i].getIcon().anchor.x,
-                  y: markers[i].getIcon().anchor.y
-                },
-                scale: updatedScale,
-                fillOpacity: 1,
-              }
-              );
-            }
-        });
-
-      }
-
-      google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', init);
--- js/data.js
+++ js/data.js
@@ -1,152 +1,178 @@
 var locationsArray = [
   {
+    title: "Warszawa",
     position: {
       lat: 52.239876, 
       lng: 21.018622
-    }, 
-    title:"Warszawa",
-    icon: icon1
+    },
   },
   {
+    title: "Siedlce",
     position: {
       lat: 52.167279, 
       lng: 22.299210
-    },
-    title:"Siedlce",
-    icon: icon1
+    }
   },
   {
+    title: "Lublin",
     position: {
       lat: 51.247131, 
       lng: 22.576816
-    },
-    title:"Lublin",
-    icon: icon1,
-    dates: [
-      {
-        from: new Date(2015, 1, 3),
-        to: new Date(2015, 1, 6)
-      }
-    ]
+    }
   },
   {
+    title: "Zamość",
     position: {
       lat: 50.722850, 
       lng: 23.254177
-    },
-    title:"Zamość",
-    icon: icon1,
-    dates: [
-      {
-        from: new Date(2015, 1, 5),
-        to: new Date(2015, 1, 5)
-      }
-    ]
+    }
   },
   {
+    title: "Olsztyn",
     position: {
       lat: 53.779764, 
       lng: 20.477715
-    },
-    title:"Olsztyn",
-    icon: icon1,
-    dates: [
-      {
-        from: new Date(2014, 11, 11),
-        to: new Date(2014, 11, 11)
-      }
-    ]
+    }
   },
   {
+    title: "Bydgoszcz",
     position: {
       lat: 53.123009, 
       lng: 18.022390
-    },
-    title:"Bydgoszcz",
-    icon: icon2
+    }
   },
   {
+    title: "Wilno",
     position: {
       lat: 54.682312, 
       lng: 25.261172
-    },
-    title:"Wilno",
-    icon: icon2
-  },
+    }
+  },  
   {
+    title: "Kowno",
     position: {
-      lat: 52.462489, 
-      lng: 16.923638
-    },
-    title:"Poznań",
-    icon: icon2
+      lat: 54.899629, 
+      lng: 23.902835
+    }
   },
   {
+    title: "Poznań",
+    position: {
+      lat: 52.407567, 
+      lng: 16.926380
+    }
+  },
+  {
+    title: "Białystok",
     position: {
       lat: 53.128861, 
       lng: 23.179412
-    },
-    title:"Białystok",
-    icon: icon1,
-    dates: [
-      {
-        from: new Date(2015, 2, 13),
-        to: new Date(2015, 2, 15)
-      }
-    ]
+    }
   },
   {
+    title: "Katowice",
     position: {
       lat: 50.261982, 
       lng: 19.037405
-    },
-    title:"Katowice",
-    icon: icon2
+    }
   },
   {
+    title: "Tykocin",
     position: {
       lat: 53.206056, 
       lng: 22.774532
-    },
-    title:"Tykocin",
-    icon: icon2
+    }
   },
   {
+    title: "Supraśl",
     position: {
       lat: 53.204882, 
       lng: 23.340239
-    },
-    title:"Supraśl",
-    icon: icon2
+    }
   },
   {
+    title: "Muntowo",
     position: {
       lat: 53.882386, 
       lng: 21.354484
-    },
-    title:"Muntowo",
-    icon: icon1,
-    dates: [
-      {
-        from: new Date(2014, 11, 8),
-        to: new Date(2014, 11, 10)
-      }
-    ]
+    }
   },
   {
+    title: "Kózki",
     position: {
       lat: 52.3577848, 
       lng: 22.871947
-    },
-    title:"Kózki",
-    icon: icon2
+    }
   },
   {
+    title: "Góra Grabarka",
     position: {
       lat: 52.4168014, 
       lng: 23.007973
-    },
-    title:"Góra Grabarka",
-    icon: icon2
-  }
+    }
+  },  
+  {
+    title: "Barcelona",
+    position: {
+      lat: 41.384981, 
+      lng: 2.183442
+    }
+  },  
+  {
+    title: "Sztokholm",
+    position: {
+      lat: 59.328519, 
+      lng: 18.070625
+    }
+  },  
+  {
+    title: "Göteborg",
+    position: {
+      lat: 57.708114, 
+      lng: 11.974702
+    }
+  },  
+  {
+    title: "Vaxholm",
+    position: {
+      lat: 59.403141, 
+      lng: 18.326638
+    }
+  },   
+  {
+    title: "Uppsala",
+    position: {
+      lat: 59.858785, 
+      lng: 17.637847
+    }
+  },  
+  {
+    title: "Ryga",
+    position: {
+      lat: 56.950432, 
+      lng: 24.096885
+    }
+  },    
+  {
+    title: "Jurmała",
+    position: {
+      lat: 56.972494, 
+      lng: 23.795477
+    }
+  },  
+  {
+    title: "Kraków",
+    position: {
+      lat: 50.065040, 
+      lng: 19.945477
+    }
+  },  
+  {
+    title: "Sanok",
+    position: {
+      lat: 49.555068, 
+      lng: 22.207307
+    }
+  },
+
 ];
